bcfe8390e6866ee8cf8eaf57730f35ad48c6ac73
Matheus Prudente <maths.henrique.14.2001@gmail.com>
2023-10-09 23:31:17 -0300

    first commit

---
+++ src/main/java/br/com/rocket/todolist/user/UserModel.java
@@ -0,0 +1,56 @@
+package br.com.rocket.todolist.user;
+
+import java.util.Objects;
+
+public class UserModel {
+
+	private String username;
+	private String name;
+	private String password;
+
+	public UserModel() {
+		super();
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name, password, username);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserModel other = (UserModel) obj;
+		return Objects.equals(name, other.name) && Objects.equals(password, other.password)
+				&& Objects.equals(username, other.username);
+	}
+}

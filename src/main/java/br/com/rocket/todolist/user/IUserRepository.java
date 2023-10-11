0df260c4944051ef4ce76237979e904248a77dfc
Matheus Prudente <maths.henrique.14.2001@gmail.com>
2023-10-11 00:06:43 -0300

    aula 2

---
+++ src/main/java/br/com/rocket/todolist/user/IUserRepository.java
@@ -0,0 +1,12 @@
+package br.com.rocket.todolist.user;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IUserRepository extends JpaRepository<UserModel, UUID> {
+
+	UserModel findByUsername(String username);
+}
--- src/main/java/br/com/rocket/todolist/user/UserController.java
+++ src/main/java/br/com/rocket/todolist/user/UserController.java
@@ -1,5 +1,7 @@
 package br.com.rocket.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+	@Autowired
+	private IUserRepository userRepository;
+
 	@PostMapping("/")
-	public void create(@RequestBody UserModel userModel) {
-		System.out.println(userModel.toString());
+	public ResponseEntity<?> create(@RequestBody UserModel userModel) {
+		var user = userRepository.findByUsername(userModel.getUsername());
+
+		if (user != null) {
+			return ResponseEntity.status(400).body("The user already exists");
+		}
+
+		var userCreated = userRepository.save(userModel);
+		return ResponseEntity.status(200).body(userCreated);
 	}
 }

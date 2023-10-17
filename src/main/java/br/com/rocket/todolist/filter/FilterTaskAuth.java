98df2b703931878a0afe4757d73aa2e4e41f21b2
Matheus Prudente <maths.henrique.14.2001@gmail.com>
2023-10-16 22:04:26 -0300

    ajuste

--- src/main/java/br/com/rocket/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/rocket/todolist/filter/FilterTaskAuth.java
@@ -25,24 +25,29 @@ public class FilterTaskAuth extends OncePerRequestFilter {
 			throws ServletException, IOException {
 		var servletPath = request.getServletPath();
 
-		if ("/tasks/".startsWith(servletPath)) {
+		if (servletPath.startsWith("/tasks/")) {
 			var authorization = request.getHeader("Authorization");
-			var authEncoded = authorization.substring("Basic".length()).trim();
-			byte[] authDecode = Base64.getDecoder().decode(authEncoded);
-			var authString = new String(authDecode);
+			var encodedAuth = authorization.substring("Basic".length()).trim();
+
+			byte[] decodedAuth = Base64.getDecoder().decode(encodedAuth);
+
+			var authString = new String(decodedAuth);
 
 			String[] credentials = authString.split(":");
 			String username = credentials[0];
 			String password = credentials[1];
 
-			var user = userRepository.findByUsername(username);
+			System.out.println("Authorization");
+			System.out.println(username);
+			System.out.println(password);
 
+			var user = this.userRepository.findByUsername(username);
 			if (user == null) {
 				response.sendError(401);
 			} else {
-				var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
-				if (passwordVerify.verified) {
-					request.setAttribute("idUser", user.getId());
+				var verifyPassword = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+				if (verifyPassword.verified) {
+					request.setAttribute("userId", user.getId());
 					filterChain.doFilter(request, response);
 				} else {
 					response.sendError(401);
@@ -51,6 +56,6 @@ public class FilterTaskAuth extends OncePerRequestFilter {
 		} else {
 			filterChain.doFilter(request, response);
 		}
-
 	}
+
 }
